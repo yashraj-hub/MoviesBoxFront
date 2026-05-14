f9a75b4a215a4b2f08998ba586335ab2325c4d5c
unknown <yashrajnayak08@gmail.com>
2026-05-14 15:11:46 +0530

    revert to relative api path

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -2,7 +2,7 @@ import { createContext, useContext, useEffect, useState, useCallback } from 'rea
 import { registerLogoutHandler } from '../utils/apiFetch'
 
 const TOKEN_KEY = 'moviesbox_token'
-const API_BASE = 'https://moviesboxbackend.onrender.com/api'
+const API_BASE = '/api'
 
 const AuthContext = createContext(null)
 
--- src/utils/apiFetch.js
+++ src/utils/apiFetch.js
@@ -1,5 +1,5 @@
 const TOKEN_KEY = 'moviesbox_token'
-const API_BASE = 'https://moviesboxbackend.onrender.com/api'
+const API_BASE = '/api'
 
 let _forceLogout = null
 
